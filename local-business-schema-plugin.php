a2bc99dd36cae2709e5a7f352db8b88ab7186865
ido barnea <barbareshet@gmail.com>
2018-01-28 10:49:55 +0200

    home page only schema

--- local-business-schema-plugin.php
+++ local-business-schema-plugin.php
@@ -203,13 +203,13 @@ function coinso_schema_content($args, $content = null){
 function custom_shortcode_scripts() {
     global $post;
     if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'schema') ) {
-            if( is_front_page() ){
+
                 add_action('wp_footer', 'coinso_footer_schema_ld_json');
-            }
+
     }
 }
 function coinso_footer_schema_ld_json(){
-
+    if( is_front_page() ) :
     ob_start(); ?>
     <script type="application/ld+json">
             {
@@ -249,6 +249,7 @@ function coinso_footer_schema_ld_json(){
     <?php
 
     echo ob_get_clean();
+    endif;
 }
 
 

138e640f1bb32f00736a7896a74d17312aff48a0
ido barnea <barbareshet@gmail.com>
2018-04-10 14:48:07 +0300

    hasMap Error
    ld+json fixed hasMap link

--- local-business-schema-plugin.php
+++ local-business-schema-plugin.php
@@ -239,7 +239,7 @@ function coinso_footer_schema_ld_json(){
             "@context": "http://schema.org",
             "@type": "<?php echo get_theme_mod( 'schema_type' ) ? get_theme_mod( 'schema_type' ) : 'LocalBusiness'; ?>",
             "image": "<?php echo get_theme_mod( 'schema_logo' ) ? get_theme_mod( 'schema_logo' ) : get_stylesheet_directory_uri() . '/assets/img/logo.png'; ?>",
-            "hasMap": "<?php echo 'https://www.google.com/maps/'. get_theme_mod( 'hasMap' ) ? 'https://www.google.com/maps/'. get_theme_mod( 'hasMap' ) : ''; ?>",
+            "hasMap": "<?php echo 'https://www.google.com/maps/@'. get_theme_mod( 'hasMap' ) ? 'https://www.google.com/maps/'. get_theme_mod( 'hasMap' ) : ''; ?>",
             "address": {
             "@type": "PostalAddress",
             "streetAddress": "<?php echo get_theme_mod( 'schema_street_address' ) ? get_theme_mod( 'schema_street_address' ) : 'Street Name' ?>",
